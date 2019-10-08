d15aba309e64c6a8da8e8c56cffe9aa815b0c9fe
mauro.fioravanzi <mauro.fioravanzi@mondadori.it>
2019-10-08 10:52:29 +0200

    its a go!

---
+++ wp-content/plugins/cfa/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: CFA customizations
+Plugin URI: http://imeuro.io/
+Description: Custom functions&features on Conceptualfinearts.com
+Version: 1.0
+Author: Mauro Fioravanzi
+Author URI: http://imeuro.io/
+*/
+
+
+include('inc/custom-post-types-fields-taxonomies.php');
+include('inc/backend-functions.php');
+
+
+?>
