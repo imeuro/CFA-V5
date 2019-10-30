4a8398bc16faac8f64ff237217a631eca1f53577
imeuro <mauro.fioravanzi@gmail.com>
2019-10-30 16:06:28 +0100

    rimossoo da functions doppia dichiarazione post types

--- wp-content/themes/CFA_v5/functions.php
+++ wp-content/themes/CFA_v5/functions.php
@@ -425,49 +425,7 @@ class CSS_Menu_Maker_Walker extends Walker {
 
 
 // Register Custom Post Type
-function CFA_custom_post_type() {
 
-	$labels = array(
-		'name'                => _x( 'CFA Authors', 'Post Type General Name', 'text_domain' ),
-		'singular_name'       => _x( 'CFA Author', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'           => __( 'CFA Authors', 'text_domain' ),
-		'name_admin_bar'      => __( 'CFA Authors', 'text_domain' ),
-		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-		'all_items'           => __( 'All Items', 'text_domain' ),
-		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
-		'add_new'             => __( 'Add New', 'text_domain' ),
-		'new_item'            => __( 'New Item', 'text_domain' ),
-		'edit_item'           => __( 'Edit Item', 'text_domain' ),
-		'update_item'         => __( 'Update Item', 'text_domain' ),
-		'view_item'           => __( 'View Item', 'text_domain' ),
-		'search_items'        => __( 'Search Item', 'text_domain' ),
-		'not_found'           => __( 'Not found', 'text_domain' ),
-		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
-	);
-	$args = array(
-		'label'               => __( 'CFA Author', 'text_domain' ),
-		'description'         => __( 'CFA Authors', 'text_domain' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'thumbnail', ),
-		'taxonomies'          => array( 'category', 'post_tag' ),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'menu_position'       => 20,
-		'menu_icon'           => 'dashicons-businessman',
-		'show_in_admin_bar'   => true,
-		'show_in_nav_menus'   => true,
-		'can_export'          => true,
-		'has_archive'         => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'capability_type'     => 'page',
-	);
-	register_post_type( 'cfa_authors', $args );
-
-}
-add_action( 'init', 'CFA_custom_post_type', 0 );
 
 //////////////////////////////////////
 //
