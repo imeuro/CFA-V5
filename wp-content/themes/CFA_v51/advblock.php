cfe88dc7136a044d0a42f83f66c7b2b14c30d813
imeuro <mauro.fioravanzi@gmail.com>
2020-05-26 11:50:23 +0200

    pic+logo def

--- wp-content/themes/CFA_v51/advblock.php
+++ wp-content/themes/CFA_v51/advblock.php
@@ -2,10 +2,10 @@
   <div class="adv-block newitem">
     <a href="https://kalliste.shop/?cid=CFA" target="_blank" class="left">
         <div class="adv-image">
-          <img src="wp-content/themes/CFA_v51/ads/kalliste-cover.png" loading="lazy" />
+          <img src="wp-content/themes/CFA_v51/ads/kalliste-cover.jpg" loading="lazy" />
         </div>
         <div class="adv-copy">
-          <p><img src="wp-content/themes/CFA_v51/ads/kalliste-logo-trasp.png" loading="lazy" /></p>
+          <p><img src="wp-content/themes/CFA_v51/ads/kalliste-logo.svg" loading="lazy" /></p>
         </div>
     </a>
   </div>
