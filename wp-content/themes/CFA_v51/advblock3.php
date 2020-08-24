e2209f3c4effa507785d1f26aef0b95c1cdee7a0
imeuro <mauro.fioravanzi@gmail.com>
2020-08-24 17:23:16 +0200

    adv block #3

---
+++ wp-content/themes/CFA_v51/advblock3.php
@@ -0,0 +1,12 @@
+<article id="post-ad-insert" class="post type-post has-post-thumbnail hentry isotope-item">
+  <div class="adv-block newitem">
+    <a href="https://kalliste.shop/?cid=CFA" target="_blank" class="left">
+        <div class="adv-image">
+          <img src="wp-content/themes/CFA_v51/ads/kalliste-cover.jpg" loading="lazy" />
+        </div>
+        <div class="adv-copy">
+          <p><img src="wp-content/themes/CFA_v51/ads/kalliste-logo.svg" loading="lazy" /></p>
+        </div>
+    </a>
+  </div>
+</article>
