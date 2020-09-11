5f1d7749953fe78c140b368528bcf67447757da1
imeuro <mauro.fioravanzi@gmail.com>
2020-09-11 14:54:59 +0200

    Wordpress as a submodule

--- wp-content/themes/CFA_v51/searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<div>
-		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" onfocus="this.value=''" />
-		<input type="submit" id="searchsubmit" value="" class="icon-magnifying-glass" />
-	</div>
-</form>
