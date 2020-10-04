4ea6910cc00ed2264538a68d22fcc4acb912c3ea
meuro <mauro.fioravanzi@mondadori.it>
2020-10-04 02:26:39 +0200

    adv in article

--- CFA-content/themes/CFA_v51/js/CFA_functions_home.js
+++ CFA-content/themes/CFA_v51/js/CFA_functions_home.js
@@ -257,7 +257,7 @@ let getADS = () => {
 	// read and inject advs in position:
 	const adslot1 = document.querySelector('#post-area article:nth-child(2)');
 	let sp1 = document.createElement("article");
-	sp1.setAttribute('class','post type-post has-post-thumbnail hentry status-publish format-adv1 post-ad-insert');
+	sp1.setAttribute('class','post type-post has-post-thumbnail hentry status-publish spformat1 post-spinsert');
 	sp1.setAttribute('id', 'advblock1');
 	let sp2 = adslot1;
 	let parentDiv = sp2.parentNode
@@ -266,7 +266,7 @@ let getADS = () => {
 
 	const adslot2 = document.querySelector('#post-area article:nth-child(5)');
 	let sp1_2 = document.createElement("article");
-	sp1_2.setAttribute('class','post type-post has-post-thumbnail hentry status-publish format-adv2 post-ad-insert');
+	sp1_2.setAttribute('class','post type-post has-post-thumbnail hentry status-publish spformat2 post-spinsert');
 	sp1_2.setAttribute('id', 'advblock2');
 	let sp2_2 = adslot2;
 	let parentDiv_2 = sp2_2.parentNode
@@ -275,7 +275,7 @@ let getADS = () => {
 
 	const adslot3 = document.querySelector('#post-area article:nth-child(8)');
 	let sp1_3 = document.createElement("article");
-	sp1_3.setAttribute('class','post type-post has-post-thumbnail hentry status-publish format-adv3 post-ad-insert');
+	sp1_3.setAttribute('class','post type-post has-post-thumbnail hentry status-publish spformat3 post-spinsert');
 	sp1_3.setAttribute('id', 'advblock3');
 	let sp2_3 = adslot3;
 	let parentDiv_3 = sp2_3.parentNode
@@ -284,7 +284,7 @@ let getADS = () => {
 
 	const adslot4 = document.querySelector('#post-area article:nth-child(11)');
 	let sp1_4 = document.createElement("article");
-	sp1_4.setAttribute('class','post type-post has-post-thumbnail hentry status-publish format-adv4 post-ad-insert');
+	sp1_4.setAttribute('class','post type-post has-post-thumbnail hentry status-publish spformat4 post-spinsert');
 	sp1_4.setAttribute('id', 'advblock4');
 	let sp2_4 = adslot4;
 	let parentDiv_4 = sp2_4.parentNode
--- CFA-content/themes/CFA_v51/single.php
+++ CFA-content/themes/CFA_v51/single.php
@@ -4,7 +4,24 @@
  */
 
 ?>
-<?php get_header(); ?>
+<?php get_header(); 
+
+$advinsert = get_posts(array(
+  'numberposts' => 1,
+  'post_status' => 'publish',
+  'post_type'   => 'cfa_sponsors',
+  'meta_key'    => 'sponsor_position',
+  //'meta_value'  => $postnum,
+  'orderby'     => 'rand',
+));
+if (!empty($advinsert)) {
+  $currentTS = time();
+  $advpost = $advinsert[0];
+  $advpics = get_field('sponsor_pics',$advpost->ID);
+  $advStart = get_field('sponsor_start_date',$advpost->ID);
+  $advEnd = get_field('sponsor_end_date',$advpost->ID);
+}
+?>
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -40,6 +57,32 @@
 						</div>
 					<?php endif; ?>
           
+
+
+        <?php
+        if (!empty($advinsert) && ($currentTS > $advStart && $currentTS < $advEnd) ) {
+        ?>
+        <section id="ADVblock-inarticle-<?php echo $advpost->post_title; ?>" class="type-post post-spinsert inarticle-ad-insert">
+          <div class="spblock newitem">
+            <a href="<?php echo get_field('sponsor_url',$advpost->ID); ?>?cid=CFA" target="_blank" rel="nofollow noopener" class="left">
+                <div class="spimage">
+                  <?php foreach ($advpics as $advpic) {
+                    $advpicsrc =  wp_get_attachment_image_src($advpic["sponsor_pic"]["ID"], 'medium' );
+                    echo '<img src="'.$advpicsrc[0].'" loading="lazy" />';
+                  }
+                  ?>
+                </div>
+                <div class="spcopy" id="<?php echo $advpost->post_title ?>">
+                  <p><img src="<?php echo get_field('sponsor_logo',$advpost->ID); ?>" loading="lazy" /></p>
+                </div>
+            </a>
+          </div>
+        </section>
+        <?php
+        }
+        ?>
+
+
           <?php if (get_field('display_automatic_summary')): ?>
             <div class="container summary-container"></div>
           <?php endif; ?>
