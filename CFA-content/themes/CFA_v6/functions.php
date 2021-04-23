20f316cd83958c19064248be0b69838bbd16a1ee
imeuro <mauro.fioravanzi@gmail.com>
2021-04-23 14:59:20 +0200

    fix LCP: defer AND async scripts

--- CFA-content/themes/CFA_v6/functions.php
+++ CFA-content/themes/CFA_v6/functions.php
@@ -75,9 +75,9 @@ function add_async_forscript($url)
     if (strpos($url, '#deferload')===false)
         return $url;
     else if (is_admin())
-        return str_replace('#deferload', '', $url);
+        return str_replace('?deferload', '', $url);
     else
-        return str_replace('#deferload', '', $url)."'  defer"; 
+        return str_replace('?deferload', '', $url)."'  defer"; 
 }
 add_filter('clean_url', 'add_async_forscript', 11, 1);
 
@@ -129,7 +129,7 @@ function CFA_scripts() {
 		//wp_register_script('jquery', false);
 		//wp_enqueue_script('jquery');
 		//wp_enqueue_script( 'Micrio', 'https://b.micr.io/micrio-2.8.min.js', array(), null, true );
-		wp_enqueue_script( 'Swiper', get_template_directory_uri() . '/js/swiper.min.js#asyncload#deferload', array(), null, true );
+		wp_enqueue_script( 'Swiper', get_template_directory_uri() . '/js/swiper.min.js#asyncload?deferload', array(), null, true );
 		wp_enqueue_script( 'CFA-functions-foglia', get_template_directory_uri() . '/js/CFA_functions_foglia.js', array( 'CFA-functions' ), null, true );
 
 	}
