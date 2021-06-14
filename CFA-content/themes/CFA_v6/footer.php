ced55966c6bcf595c8b27b07c57d458bc7c63a81
imeuro <mauro.fioravanzi@gmail.com>
2021-06-14 23:25:04 +0200

    tolto delay analytics

--- CFA-content/themes/CFA_v6/footer.php
+++ CFA-content/themes/CFA_v6/footer.php
@@ -41,13 +41,11 @@ src="https://www.facebook.com/tr?id=508657780012788&ev=PageView
 
 <!-- Google Tag Manager -->
 <script>
-setTimeout(function(){
 	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-5MLKFBX');
-}, 2000);
 </script>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5MLKFBX"
